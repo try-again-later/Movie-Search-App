1cdf06d314048c101ad2d1d16b13b089fd178e91
pa-artem <pa.artem20@yandex.ru>
2021-12-09 17:08:58 +0300

    Remove scroll listeners on useScroll unmount

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -1,15 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 
 const useScroll = (): {x:number, y:number} => {
   const [x, setX] = useState(window.scrollX);
   const [y, setY] = useState(window.scrollY);
 
-  useEffect(() => {
-    document.addEventListener('scroll', () => {
-      setX(window.scrollX);
-      setY(window.scrollY);
-    });
+  const onScroll = useCallback(() => {
+    setX(window.scrollX);
+    setY(window.scrollY);
   }, []);
+
+  useEffect(() => {
+    document.addEventListener('scroll', onScroll);
+    return () => {
+      document.removeEventListener('scroll', onScroll);
+    };
+  }, [onScroll]);
 
   return { x, y };
 };
